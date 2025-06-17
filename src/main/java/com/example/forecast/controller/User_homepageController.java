2c9d50442c34ee6fb707d966af869c9d52cf8c4f
デン <xinru.tian@tcs.com>
2025-06-17 14:48:37 +0900

    add login and homepage

---
+++ src/main/java/com/example/forecast/controller/User_homepageController.java
@@ -0,0 +1,21 @@
+package com.example.forecast.controller;
+
+import com.example.forecast.model.User;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class User_homepageController {
+
+    @GetMapping("/user_homepage")
+    public String userHomepage(HttpSession session, Model model) {
+        Object userObj = session.getAttribute("loggedInUser");
+        if (userObj instanceof User user) {
+            model.addAttribute("username", user.getName());
+        }
+        return "user_homepage";
+    }
+}
+
