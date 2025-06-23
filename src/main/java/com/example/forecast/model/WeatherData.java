e6c1a527c5097b3618865eedbc1cf2a687057f33
mimitabu1212 <555krssy@gmail.com>
2025-06-23 13:50:43 +0900

    販売入力と実績・天気データの同期

--- src/main/java/com/example/forecast/model/WeatherData.java
+++ src/main/java/com/example/forecast/model/WeatherData.java
@@ -20,7 +20,16 @@ public class WeatherData implements Serializable {
     @Column(name = "weather_info")
     private String weatherInfo;
 
-    @Transient // DBには存在しないが画面表示などで使いたい
+    @Column(name = "weather_water")
+    private Double water;
+
+    @Column(name = "weather_wind")
+    private Double wind;
+
+    @Column(name = "weather_temperature")
+    private Double temperature;
+
+    @Transient
     private Integer totalSales;
 
     public WeatherData() {
@@ -46,6 +55,30 @@ public class WeatherData implements Serializable {
 
     public void setWeatherInfo(String weatherInfo) {
         this.weatherInfo = weatherInfo;
+    }
+
+    public Double getWater() {
+        return water;
+    }
+
+    public void setWater(Double water) {
+        this.water = water;
+    }
+
+    public Double getWind() {
+        return wind;
+    }
+
+    public void setWind(Double wind) {
+        this.wind = wind;
+    }
+
+    public Double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(Double temperature) {
+        this.temperature = temperature;
     }
 
     public Integer getTotalSales() {
